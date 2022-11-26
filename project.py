edf32b29e37cdc6f22f9a4ea2e104a54b81e2e4b
DaniilTiunov <114344853+DaniilTiunov@users.noreply.github.com>
2022-11-26 20:35:46 +0500

    TablicaVokne

--- project.py
+++ project.py
@@ -1,11 +1,23 @@
 import pandas as pd
+import tkinter as tk
+from tkinter import *
+import numpy as np
 from openpyxl import Workbook
 
+datatable = pd.read_excel('439_2frem.xls')
 
-wb = Workbook()
-df = pd.read_excel('439_2frem.xls', engine = 'openpyxl')
+window = tk.Tk()
+window.title('Table')
+label = Label(window, text = datatable)
+label.grid(column = 0, row = 0)
 
 
 
 
-type(df)
+
+
+
+window.mainloop()
+
+
+
