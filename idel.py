fefee144acf772624fc7393f6c76d479df8cf0a0
GHDVLP <f_b2002@bk.ru>
2022-12-10 15:38:47 +0500

    Сохранение полезных ссылок по PSGUI

--- idel.py
+++ idel.py
@@ -1,1 +1,15 @@
-print("Helo world")
+import PySimpleGUI as sg
+'''
+sg.theme('DarkBlue')
+layout = [ [sg.Button('Регистрация'), sg.Button('Авторизация')]
+
+]
+window = sg.Window('Window tittle', layout)
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+    print('Вы вошли', values[0])
+window.close()
+'''
+event, values = sg.Window('Get filename example', [[sg.Text('Filename')], [sg.Input(), sg.FileBrowse()], [sg.OK(), sg.Cancel()] ]).read(close=True)
